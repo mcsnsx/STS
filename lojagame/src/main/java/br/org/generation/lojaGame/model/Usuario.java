93dcb474c55f125cea7be6225b24fb7d398b414a
mcsnsx <mcs_nasc@hotmail.com>
2021-11-12 20:12:07 -0300

    Atualização Loja de Games 12.11

---
+++ lojagame/src/main/java/br/org/generation/lojaGame/model/Usuario.java
@@ -0,0 +1,82 @@
+package br.org.generation.lojaGame.model;
+
+import java.time.LocalDate;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+
+@Entity
+@Table(name = "tb_usuarios")
+public class Usuario {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private long id;
+	
+	@NotNull(message = "Esse campo é obrigatório!")
+	private String nome;
+	
+	@NotNull(message = "Esse campo é obrigatório!")
+	@Email(message = "Esse campo é obrigatório!")
+	private String usuario;
+	
+	@NotBlank(message = "Esse campo é obrigatório!")
+	@Size(min = 8, message = "A senha deve conter no mínimo 8 caracteres!")
+	private String senha;
+	
+	@Column(name = "data_nascimento")
+	@JsonFormat(pattern = "yyyy-MM-dd")
+	@NotNull(message = "Esse campo é obrigatório!")
+	private LocalDate dataNascimento;
+
+	public LocalDate getDataNascimento() {
+		return dataNascimento;
+	}
+
+	public void setDataNascimento(LocalDate dataNascimento) {
+		this.dataNascimento = dataNascimento;
+	}
+
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getUsuario() {
+		return usuario;
+	}
+
+	public void setUsuario(String usuario) {
+		this.usuario = usuario;
+	}
+
+	public String getSenha() {
+		return senha;
+	}
+
+	public void setSenha(String senha) {
+		this.senha = senha;
+	}
+
+}
---
+++ lojagame/src/main/java/br/org/generation/lojaGame/repository/UsuarioRepository.java
@@ -0,0 +1,15 @@
+package br.org.generation.lojaGame.repository;
+
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import br.org.generation.lojaGame.model.Usuario;
+
+@Repository
+public interface UsuarioRepository extends JpaRepository<Usuario, Long>{
+	
+	public Optional<Usuario> findByUsuario (String usuario);
+
+}
